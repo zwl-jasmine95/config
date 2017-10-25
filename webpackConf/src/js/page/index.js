98b8db71b4066ae84b2c30e6b611959ee4e2df73
曾文莉 <zengwenli95@gmail.com>
2017-10-25 11:26:47 +0800

    添加默认的html文件

--- webpackConf/src/js/page/index.js
+++ webpackConf/src/js/page/index.js
@@ -1,7 +1,6 @@
-import '../../sass/a.scss'
+import '../../sass/index.scss'
 import axios from 'axios'
-import jquery from 'jquery'
-console.log(jquery('h1'));
+console.log($('h1'));
 // axios.get(
 //     '/api/articles/page?code=apply_article_document&size=10&page=1'
 // ).then(res => {
--- webpackConf/webpack/webpack.config.babel.js
+++ webpackConf/webpack/webpack.config.babel.js
@@ -8,83 +8,93 @@ const path = require('path')
 const glob = require('glob'); //匹配文件路径
 
 const globPath = {
-    js:resolve('src/js/**/*.js'),
+    js: resolve('src/js/**/*.js'),
     // js:resolve('src/js/**/*.+(t|j)s'),
-    pug:resolve('src/view/page/**/*.pug')
+    pug: resolve('src/view/page/**/*.pug')
 }
 const pathJs = getEntry(globPath.js);
 const pathPug = getEntry(globPath.pug);
 
 //“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
 function resolve(...dir) {  //拼接
-   return path.resolve(__dirname, '..', ...dir)
+    return path.resolve(__dirname, '..', ...dir)
 }
 
 const webpackConf = {
-   devtool: 'eval-source-map',
-   entry: pathJs,
-   output: {
-       filename: 'js/[name].[hash:8].js',   //打包后输出文件的文件名
-       path: resolve('dist'),  //打包后文件存放的地方
-       publicPath: '/'   //发布路径配置
-   },
-   module: {
-       rules: [
-           {
-               test: /\.js$/,
-               loader: 'babel-loader'
-           },
-           {
-               test:/\.css$/,
-               loader:['css-loader','style-loader']
-           },
-           {
-               test:/\.scss$/,
-               use: extractTextPlugin.extract({
-                   use: ['css-loader','postcss-loader','sass-loader'] 
-               }) 
-               //sass-loader依赖于node-sass
-           },
-           {
-               test:/\.pug$/,
-               use:[
-                   'raw-loader',
-                   {
-                       loader:'pug-html-loader',
-                       options:{
-                           doctype:'html'
-                       }
-                   }
-               ]
-           }
-       ]
-   },
-   plugins:[
-    //start 提取公共js
-    new webpack.optimize.CommonsChunkPlugin({
-        name: "common",
-        minChunks: 2
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: "common",
-        chunks:['common']
-    }),
-    //end 提取公共js
+    devtool: 'eval-source-map',
+    entry: Object.assign(pathJs,{'vendor':['jquery']}),
+    output: {
+        filename: 'js/[name].[hash:8].js',   //打包后输出文件的文件名
+        path: resolve('dist'),  //打包后文件存放的地方
+        publicPath: '/'   //发布路径配置
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['css-loader', 'style-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: extractTextPlugin.extract({
+                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                })
+                //sass-loader依赖于node-sass
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    'raw-loader',
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            doctype: 'html'
+                        }
+                    }
+                ]
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                }, {
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
+        ]
+    },
+    plugins: [
+        //start 提取公共js
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: Infinity
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "common",
+            minChunks:2
+        }),
+        //end 提取公共js
 
-    new extractTextPlugin({
-        filename:'css/[name].css',
-        allChunks:true
-    })
-   ],
-   devServer:{
-       host:'172.16.0.105',  //代理地址-本机ip地址
-       port:80,  //端口号
-       proxy:{
-           '/api':{
-               target:''   //跨域访问地址
-           }
-       }
-   }
+        new extractTextPlugin({
+            filename: 'css/[name].css',
+            allChunks: true
+        })
+    ],
+    devServer: {
+        host: '172.16.0.105',  //代理地址-本机ip地址
+        port: 80,  //端口号
+        proxy: {
+            '/api': {
+                target: ''   //跨域访问地址
+            }
+        }
+    }
 
 }
 /**
@@ -93,9 +103,9 @@ const webpackConf = {
  * @return {obj} entries 文件名与文件路径的值键对 
  * {'a','src/js/page/a.js'}
  */
-function getEntry(globPath){
+function getEntry(globPath) {
     let entries = {};
-    glob.sync(globPath).forEach(entry=>{
+    glob.sync(globPath).forEach(entry => {
         let name = entry.split('page/')[1].split('.')[0];
         entries[name] = entry;
     });
@@ -103,11 +113,11 @@ function getEntry(globPath){
 }
 
 //pug、sass、js一一对应
-for(let pug in pathPug){
+for (let pug in pathPug) {
     let htmlWebpack = new htmlWebpackPlugin({
         filename: pug + '.html',
         template: pathPug[pug],
-        chunks:[pug, 'common']  //忽略的话所有入口都会被注入
+        chunks: [pug, 'common','vendor']  //忽略的话所有入口都会被注入
     })
     webpackConf.plugins.push(htmlWebpack);
 }
