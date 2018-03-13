2fac56294f9bda89175324c7e46ce88ce373d6ea
zwl-jasmine95 <zwl.jasmine95@gmail.com>
2018-03-13 16:12:09 +0800

    webpack 4 单页面配置

---
+++ webpackConfig/src/js/hello.js
@@ -0,0 +1,4 @@
+import '../scss/hello.scss'
+document.querySelector('h1').addEventListener('click',() => {
+    alert('哈哈哈')
+})
---
+++ webpackConfig/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+//为html文件中引入的外部资源;可以生成创建html入口文件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//该插件的主要是为了抽离css样式,防止将样式打包在js中引起页面样式加载错乱的现象
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const resolve = function resolve(...dir) {   //拼接路径
+    return path.resolve(__dirname, ...dir)
+}
+
+module.exports = {
+    entry: resolve('src/js/hello.js'),
+    output: {
+        filename: 'js/hello.min.js',
+        path: resolve('dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            },
+            {
+                test:/\.scss$/,
+                use:ExtractTextPlugin.extract({
+                    use: ['css-loader','sass-loader']
+                })
+            },
+            {
+                test:/\.js$/,
+                use:'babel-loader'
+            }
+        ]
+    },
+    plugins:[
+        new ExtractTextPlugin({
+            filename:'css/hello.min.css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'hello.html',
+            template: './src/view/hello.html'
+        })
+    ]
+}
